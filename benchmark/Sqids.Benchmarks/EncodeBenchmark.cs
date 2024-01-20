527af30b70cee379e3cae28459c5e484d6b9cbce
Ian Qvist <qvistian@gmail.com>
2024-01-20 20:17:13 +0100

    Add more benchmark cases to catch performance edge-cases

--- benchmark/Sqids.Benchmarks/EncodeBenchmark.cs
+++ benchmark/Sqids.Benchmarks/EncodeBenchmark.cs
@@ -12,8 +12,20 @@ public class EncodeBenchmark
 #endif
 
 	[Benchmark]
-	public string Encode() => _encoder.Encode(42);
+	public string EncodeSmall() => _encoder.Encode(42);
 
 	[Benchmark]
-	public IReadOnlyList<int> Decode() => _encoder.Decode("Jg");
+	public string EncodeBig() => _encoder.Encode(int.MaxValue);
+
+	[Benchmark]
+	public string EncodeMany() => _encoder.Encode(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+	[Benchmark]
+	public IReadOnlyList<int> DecodeSmall() => _encoder.Decode("Jg");
+
+	[Benchmark]
+	public IReadOnlyList<int> DecodeBig() => _encoder.Decode("UKrsQ1F");
+
+	[Benchmark]
+	public IReadOnlyList<int> DecodeMany() => _encoder.Decode("hwB5vcCxfAyBnVKMtAaV");
 }
