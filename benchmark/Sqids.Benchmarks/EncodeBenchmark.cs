3a8c4e96e12bb94a2bda05a5862d1acda12a3acf
Ian Qvist <qvistian@gmail.com>
2024-01-20 02:09:13 +0100

    Add benchmarks using BenchmarkDotNet
    It runs with all supported runtimes
    It uses memory diagnoser to check allocations

---
+++ benchmark/Sqids.Benchmarks/EncodeBenchmark.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Sqids.Benchmarks;
+
+[MemoryDiagnoser]
+public class EncodeBenchmark
+{
+#if NET7_0_OR_GREATER
+	private SqidsEncoder<int> _encoder = new SqidsEncoder<int>();
+#else
+	private SqidsEncoder _encoder = new SqidsEncoder();
+#endif
+
+	[Benchmark]
+	public string Encode() => _encoder.Encode(42);
+
+	[Benchmark]
+	public IReadOnlyList<int> Decode() => _encoder.Decode("Jg");
+}
